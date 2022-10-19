ae1f354af27f9f782f4a1afed837d3ea0658f634
Linus Foxell <linusfoxell@hotmail.com>
2022-10-19 07:35:34 +0200

    test: timer + fixing modal

--- src/components/CreateProjectModal.tsx
+++ src/components/CreateProjectModal.tsx
@@ -1,10 +1,11 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 
 interface Props {
   setModal: (modal: boolean) => void;
 }
 
 const CreateProjectModal: FC<Props> = ({ setModal }) => {
+  const [selectColor, setSelectColor] = useState<string>("green");
   return (
     <div className="flex justify-center items-center absolute top-0 right-0 bottom-0 left-0">
       <div
@@ -12,27 +13,50 @@ const CreateProjectModal: FC<Props> = ({ setModal }) => {
         onClick={() => setModal(false)}
       />
 
-      <div className="w-4/5 h-2/3 bg-slate-100 flex flex-col items-center rounded-md z-20">
-        <div className="inline-block relative w-28">
-          <label htmlFor=""></label>
-          <select className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-            <option value={"green"}>Green</option>
-            <option value={"red"}>Red</option>
-            <option value={"blue"}>Blue</option>
-            <option value={"purple"}>Purple</option>
-            <option value={"orange"}>Orange</option>
-          </select>
-          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-            <svg
-              className="fill-current h-4 w-4"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-            >
-              <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-            </svg>
+      <div className="w-4/5 h-2/3 bg-slate-100 flex flex-col items-center rounded-md z-20 relative">
+        <div className="flex flex-col justify-center items-center mt-14">
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+            Project name
+          </label>
+          <div>
+            <input
+              type="text"
+              id="project_name"
+              className="bg-white border border-gray-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 shadow"
+              placeholder="Example Project"
+              required
+            />
           </div>
         </div>
-        <div className="flex gap-2 w-[90%]">
+
+        <div className="flex flex-col justify-center items-center mt-5">
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+            Color for your project
+          </label>
+          <div className="inline-block relative w-28">
+            <select
+              className="block appearance-none w-full bg-white border border-gray-400 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+              onChange={(e) => setSelectColor(e.target.value)}
+            >
+              <option value={"green"}>Green</option>
+              <option value={"red"}>Red</option>
+              <option value={"blue"}>Blue</option>
+              <option value={"purple"}>Purple</option>
+              <option value={"orange"}>Orange</option>
+            </select>
+            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+              <svg
+                className="fill-current h-4 w-4"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+              </svg>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex gap-2 w-[90%] absolute bottom-10">
           <button className="bg-green-600 p-3 rounded-md mt-10 w-full text-white font-semibold">
             Create
           </button>
--- src/pages/timer.tsx
+++ src/pages/timer.tsx
@@ -1,7 +1,31 @@
+import { useStoreContext } from "../context";
+
 function Timer() {
+  const { timelogs } = useStoreContext();
+
+  const seconds = 3699;
+
+  const timerHours = (seconds: number) => {
+    return Math.floor(seconds / 3600);
+  };
+
+  const timerMinutes = (seconds: number) => {
+    return Math.floor((seconds / 60) % 60);
+  };
+
   return (
     <div>
       <div>TIMER</div>
+      {timelogs &&
+        timelogs.map((item) => {
+          const hehe = new Date(Date.parse(item.createdAt));
+          return <div key={item.id}>{hehe.toUTCString()}</div>;
+        })}
+
+      <div>
+        {timerHours(seconds)} hours {timerMinutes(seconds)} minutes{" "}
+        {seconds % 60} seconds
+      </div>
     </div>
   );
 }
